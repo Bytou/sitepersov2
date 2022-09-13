62a4bd823904c2a479a37e7857272748d5b264b5
Antoine Martet <antoine.martet@gmail.com>
2022-09-13 19:02:17 +0200

    Components hierarchy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes,RouterModule } from '@angular/router';
-import { FormationComponent } from './components/formation/formation.component';
+import { FormationComponent } from './components/subsection/formation/formation.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProComponent } from './components/subsection/pro/pro.component';
+import { AssoComponent } from './components/subsection/asso/asso.component';
+import { TechnoComponent } from './components/subsection/techno/techno.component';
+import { ContactComponent } from './components/subsection/contact/contact.component';
 
 const routes: Routes = [
   { path: "formation", component: FormationComponent},
+  { path: "pro", component: ProComponent},
+  { path: "asso", component: AssoComponent},
+  { path: "techno", component: TechnoComponent},
+  { path: "contact", component: ContactComponent},
   { path: "", component: HomeComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProComponent } from './components/subsection/pro/pro.component';
+import { AssoComponent } from './components/subsection/asso/asso.component';
+import { TechnoComponent } from './components/subsection/techno/techno.component';
+import { ContactComponent } from './components/subsection/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProComponent,
+    AssoComponent,
+    TechnoComponent,
+    ContactComponent
 ],
   imports: [
     BrowserModule,
