f4f6db4fc3f5c4511d16c88789d0dd6496b81c9e
Antoine Martet <antoine.martet@gmail.com>
2022-10-13 18:09:09 +0200

    Update Formation class (inherit of Basic class)

--- src/models/formation.ts
+++ src/models/formation.ts
@@ -1,5 +1,6 @@
+import { Basic } from "./basic";
 
-export class Formation {
+export class Formation extends Basic {
 
     title : string;
     begin : Date;
@@ -10,6 +11,7 @@ export class Formation {
     desc : string; 
 
     constructor(title: string, begin: Date, end: Date, etablishment: string, city: string, postcode: number, desc:string) {
+        super()
         this.title = title;
         this.begin = begin;
         this.end = end; 
@@ -17,6 +19,14 @@ export class Formation {
         this.city = city;
         this.postcode = postcode;
         this.desc = desc;
+    }
+
+    getEndToString(): String {
+        return this.dateToString(this.end)
+    }
+    
+    getBeginToString(): String {
+        return this.dateToString(this.begin)
     }
 
 }
