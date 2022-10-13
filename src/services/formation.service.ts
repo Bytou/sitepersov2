3b98d719c3b542496da20875c28d8d0a74a0ac83
Antoine Martet <antoine.martet@gmail.com>
2022-10-13 18:22:29 +0200

    Creation of Services (Basic and Formations)

---
+++ src/services/formation.service.ts
@@ -0,0 +1,14 @@
+import { Formation } from "src/models/formation";
+import { BasicService } from "./basic.service";
+
+export class FormationService extends BasicService{
+
+    getFormations(): Array<Formation> {
+        let list = new Array<Formation>;
+        list.push(new Formation("Diplôme d'Ingénieur Généraliste", new Date("2016-09-01"), new Date("2022-09-01"), 'Ecole Nationale D\'Ingénieurs de Brest', 'Brest', 29200, 'test'));
+        list.push(new Formation("Baccalauréat S, Mention Européenne", new Date("2013-09-01"), new Date("2016-09-01"), 'Lycée Gaspard Monge', 'Savigny Sur Orge', 91600, 'test'));
+        return list;
+    }
+
+
+}
