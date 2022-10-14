122c2a114d25e01c574c7678f8bd7e5d43be3c4a
Antoine Martet <antoine.martet@gmail.com>
2022-10-14 12:09:02 +0200

    Add Formation sevices and link to component

--- src/services/formation.service.ts
+++ src/services/formation.service.ts
@@ -3,8 +3,9 @@ import { BasicService } from "./basic.service";
 
 export class FormationService extends BasicService{
 
-    getFormations(): Array<Formation> {
-        let list = new Array<Formation>;
+
+    static getFormations(): Array<Formation> {
+        let list = new Array<Formation>();
         list.push(new Formation("Diplôme d'Ingénieur Généraliste", new Date("2016-09-01"), new Date("2022-09-01"), 'Ecole Nationale D\'Ingénieurs de Brest', 'Brest', 29200, 'test'));
         list.push(new Formation("Baccalauréat S, Mention Européenne", new Date("2013-09-01"), new Date("2016-09-01"), 'Lycée Gaspard Monge', 'Savigny Sur Orge', 91600, 'test'));
         return list;
